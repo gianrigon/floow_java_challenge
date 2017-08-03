37044038b556c8d8242ba44228e11057150e7108
Gianluca Rigon <drgianlucarigon@gmail.com>
2017-08-03 19:35:55 +0200

    WIP

--- src/com/thefloow/challenge/TextAnalyzer.java
+++ src/com/thefloow/challenge/TextAnalyzer.java
@@ -2,12 +2,16 @@ package com.thefloow.challenge;
 
 import java.util.Properties;
 
+import com.mongodb.client.MongoDatabase;
+
 public class TextAnalyzer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub		
 		java.util.concurrent.Semaphore sem = new java.util.concurrent.Semaphore(10, true);
 		sem.acquireUninterruptibly();
+		com.mongodb.MongoClient m = new com.mongodb.MongoClient("localhost", 21321);
+		MongoDatabase db =  m.getDatabase("fake");		
 	}
 
 }
