51f47bca0a78e55f2b0e6582a6cc797a50fa4f02
Gianluca Rigon <drgianlucarigon@gmail.com>
2017-08-03 18:37:58 +0200

    Added MongoDB support

--- src/com/thefloow/challenge/TextAnalyzer.java
+++ src/com/thefloow/challenge/TextAnalyzer.java
@@ -1,10 +1,13 @@
 package com.thefloow.challenge;
 
+import java.util.Properties;
+
 public class TextAnalyzer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stub		
+		java.util.concurrent.Semaphore sem = new java.util.concurrent.Semaphore(10, true);
+		sem.acquireUninterruptibly();
 	}
 
 }
